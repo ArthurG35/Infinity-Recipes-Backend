d06f8339bc0a665e1b5abd871163f8b8ac5933da
ArthurG35 <arthur.gougeon@epsi.com>
2022-10-01 18:28:52 +0200

    Init

---
+++ src/main/java/fr/InfinityRecipes/InfinityRecipesApplication.java
@@ -0,0 +1,13 @@
+package fr.InfinityRecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityRecipesApplication.class, args);
+	}
+
+}
